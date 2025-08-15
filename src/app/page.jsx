e982cbbc366bcb690bbdd5de5a9222ac9d2007ae
wakamonoo <joven.serdanbataller21@gmail.com>
2025-08-15 18:51:48 +0800

    kasurog initialization

---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center h-screen">
+      <h1 className="text-black">kasurog initialization</h1>
+
+    </div>
+  );
+}
