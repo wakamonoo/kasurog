25e51ec2faa369bef243361a96e968531a895678
wakamonoo <joven.serdanbataller21@gmail.com>
2025-08-16 13:18:18 +0800

    arqila nav

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,30 @@
+"use client";
 import Image from "next/image";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="flex justify-center h-screen">
-      <h1 className="text-black">kasurog initialization</h1>
+  const [hideNav, setHideNav] = useState(false);
 
-    </div>
+  return (
+    <>
+      <div className="flex justify-between p-4 h-screen text-header text-2xl font-header">
+        <h1 className="">arqila.</h1>
+        <div>
+          <button onClick={() => setHideNav((prev) => !prev)}>
+            <FaBars className="duration-200 hover:scale-110 active:scale-110 cursor-pointer" />
+          </button>
+        </div>
+      </div>
+      {hideNav && (
+        <div className="inset-0 fixed backdrop-blur-xs z-50">
+          <div className="h-screen shadow-2xl fixed z-50 top-0 right-0 w-[60vw] bg-panel p-4">
+            <button onClick={() => setHideNav(false)}>
+              <FaTimes className="text-xl text-header duration-200 hover:scale-110 active:scale-110 cursor-pointer" />
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
